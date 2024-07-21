6a8ba8db59d3388d7276a188f617102abc67e0f3
Luca Sichi <luca.sichi@hotmail.com>
2024-07-21 10:40:53 +0200

    transformer

--- notebooks/preprocessing.py
+++ notebooks/preprocessing.py
@@ -2,6 +2,10 @@ from ekphrasis.classes.preprocessor import TextPreProcessor
 from ekphrasis.classes.tokenizer import SocialTokenizer
 from ekphrasis.dicts.emoticons import emoticons
 import re
+from tqdm import tqdm
+
+def replace_consecutve_whitespaces(input_string:str):
+    return re.sub(r'\s+', ' ', input_string)
 
 def process_string(input_string:str):
     # Find all hashtags
@@ -14,9 +18,9 @@ def process_string(input_string:str):
     parts = [part.strip() for part in parts if part.strip()]
     
     # Format the output list
-    hashtags_written_out = input_string.replace('<hashtag> ', '').replace('</hashtag> ', '')
-    no_hashtags = ''.join(parts) if '<hashtag>' in input_string else ''
-    output = [hashtags_written_out] + [no_hashtags] + [hashtags]
+    hashtags_written_out = input_string.replace('<hashtag> ', '').replace('</hashtag>', '')
+    no_hashtags = ' '.join(parts) if '<hashtag>' in input_string else ''
+    output = [replace_consecutve_whitespaces(hashtags_written_out)] + [replace_consecutve_whitespaces(no_hashtags)] + [hashtags]
     
     return output
 
@@ -34,8 +38,17 @@ def preprocess(tweets:list, normalize=['url', 'email', 'percent', 'money', 'phon
     normalize=normalize,
     annotate={'hashtag'},
     )
-    pre_process_1 = [" ".join(text_processor.pre_process_doc(tweet)) for tweet in tweets]
-    pre_process_2 = [process_string(tweet) for tweet in pre_process_1]
+    pre_process_1 = []
+    for i in tqdm(range(len(tweets))):
+        pre_process_1.append(" ".join(text_processor.pre_process_doc(tweets[i].replace('\n', ''))))
+    pre_process_2 = []
+    for i in tqdm(range(len(pre_process_1))):
+        pre_process_2.append(process_string(pre_process_1[i]))
     
     return pre_process_2
     
+def main():
+    print(preprocess(["I love #pizza", "I love #pasta", "I love #food"]))
+
+if __name__ == '__main__':
+    main()
